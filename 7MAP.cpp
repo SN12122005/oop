9c30aed7c5c07fcc4c458a67c5aad5875de4c3ab
SN12122005 <surajnwork108@gmail.com>
2024-12-18 22:34:59 +0530

    Update 7MAP.cpp

---
+++ 7MAP.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+    map<string, int> states = {{"Maharashtra", 120000000}, {"Karnataka", 60000000},{"Goa" , 4500000}};
+    string stateName;
+
+    cout << "Enter state name: ";
+    cin >> stateName;
+
+    auto it = states.find(stateName);
+    if (it != states.end()) {
+        cout << "Population of " << stateName << ": " << it->second << endl;
+    } else {
+        cout << "State not found." << endl;
+    }
+
+    return 0;
+}
